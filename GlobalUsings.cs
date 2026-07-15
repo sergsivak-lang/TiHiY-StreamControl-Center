c32b62522cb6e5358ffa14699524e0deb5c08b27
TiHiY-DED <234259815+sergsivak-lang@users.noreply.github.com>
2026-07-16 02:16:15 +0300

    Add global WPF data binding namespace

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -10,6 +10,7 @@ global using System.Threading.Tasks;
 global using System.Windows;
 global using System.Windows.Controls;
 global using System.Windows.Controls.Primitives;
+global using System.Windows.Data;
 global using System.Windows.Input;
 global using System.Windows.Media;
 global using System.Windows.Threading;
