3129a7a1d2618748b6d6704f75d050a03cec7f4e
TiHiY-DED <234259815+sergsivak-lang@users.noreply.github.com>
2026-08-07 21:55:32 +0300

    Add TiHiY Game Mode entry point

---
+++ tools/TiHiY.GameMode/Program.cs
@@ -0,0 +1,11 @@
+namespace TiHiY.GameMode;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
